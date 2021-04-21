0e6c07edc6ed71c00152bcf8c2f6e90e48ba5a5a
IrfanDanish <irdanish11@gmail.com>
2021-04-21 12:05:40 +0500

    added the code

---
+++ core/utils.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 30 18:06:17 2020
+
+@author: danish
+"""
+
+import cv2
+import random
+import colorsys
+import numpy as np
+import tensorflow as tf
+from core.config import cfg
+
+
+def get_anchors(anchors_path, tiny=False):
+    anchors = np.array(anchors_path)
+    if tiny:
+        return anchors.reshape(2, 3, 2)
+    else:
+        return anchors.reshape(3, 3, 2)
+
+def read_class_names(class_file_name):
+    names = {}
+    with open(class_file_name, 'r') as data:
+        for ID, name in enumerate(data):
+            names[ID] = name.strip('\n')
+    return names
+
+def load_config(FLAGS_Model='yolov4', FLAGS_TINY=False):
+    if FLAGS_TINY:
+        STRIDES = np.array(cfg.YOLO.STRIDES_TINY)
+        ANCHORS = get_anchors(cfg.YOLO.ANCHORS_TINY, FLAGS_TINY)
+        XYSCALE = cfg.YOLO.XYSCALE_TINY
+    else:
+        STRIDES = np.array(cfg.YOLO.STRIDES)
+        ANCHORS = get_anchors(cfg.YOLO.ANCHORS, FLAGS_TINY)
+        XYSCALE = cfg.YOLO.XYSCALE
+    NUM_CLASS = len(read_class_names(cfg.YOLO.CLASSES))
+    return STRIDES, ANCHORS, NUM_CLASS, XYSCALE
+
+# helper function to convert bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, xmax, ymax
+def format_boxes(bboxes, image_height, image_width):
+    for box in bboxes:
+        ymin = int(box[0] * image_height)
+        xmin = int(box[1] * image_width)
+        ymax = int(box[2] * image_height)
+        xmax = int(box[3] * image_width)
+        box[0], box[1], box[2], box[3] = xmin, ymin, xmax, ymax
+    return bboxes
+
+def read_class_names(class_file_name):
+    names = {}
+    with open(class_file_name, 'r') as data:
+        for ID, name in enumerate(data):
+            names[ID] = name.strip('\n')
+    return names
+
+def draw_bbox(image, bboxes, info = False, counted_classes = None, show_label=True, allowed_classes=list(read_class_names(cfg.YOLO.CLASSES).values()), read_plate = False):
+    classes = read_class_names(cfg.YOLO.CLASSES)
+    num_classes = len(classes)
+    image_h, image_w, _ = image.shape
+    hsv_tuples = [(1.0 * x / num_classes, 1., 1.) for x in range(num_classes)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)), colors))
+
+    random.seed(0)
+    random.shuffle(colors)
+    random.seed(None)
+
+    out_boxes, out_scores, out_classes, num_boxes = bboxes
+    for i in range(num_boxes):
+        if int(out_classes[i]) < 0 or int(out_classes[i]) > num_classes: continue
+        coor = out_boxes[i]
+        fontScale = 0.5
+        score = out_scores[i]
+        class_ind = int(out_classes[i])
+        class_name = classes[class_ind]
+        if class_name not in allowed_classes:
+            continue
+        else:
+            if read_plate:
+                height_ratio = int(image_h / 25)
+                plate_number = recognize_plate(image, coor)
+                if plate_number != None:
+                    cv2.putText(image, plate_number, (int(coor[0]), int(coor[1]-height_ratio)), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1.25, (255,255,0), 2)
+
+            bbox_color = colors[class_ind]
+            bbox_thick = int(0.6 * (image_h + image_w) / 600)
+            c1, c2 = (coor[0], coor[1]), (coor[2], coor[3])
+            cv2.rectangle(image, c1, c2, bbox_color, bbox_thick)
+
+            if info:
+                print("Object found: {}, Confidence: {:.2f}, BBox Coords (xmin, ymin, xmax, ymax): {}, {}, {}, {} ".format(class_name, score, coor[0], coor[1], coor[2], coor[3]))
+
+            if show_label:
+                bbox_mess = '%s: %.2f' % (class_name, score)
+                t_size = cv2.getTextSize(bbox_mess, 0, fontScale, thickness=bbox_thick // 2)[0]
+                c3 = (c1[0] + t_size[0], c1[1] - t_size[1] - 3)
+                cv2.rectangle(image, c1, (np.float32(c3[0]), np.float32(c3[1])), bbox_color, -1) #filled
+
+                cv2.putText(image, bbox_mess, (c1[0], np.float32(c1[1] - 2)), cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale, (0, 0, 0), bbox_thick // 2, lineType=cv2.LINE_AA)
+
+            if counted_classes != None:
+                height_ratio = int(image_h / 25)
+                offset = 15
+                for key, value in counted_classes.items():
+                    cv2.putText(image, "{}s detected: {}".format(key, value), (5, offset),
+                            cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 2)
+                    offset += height_ratio
+    return image
